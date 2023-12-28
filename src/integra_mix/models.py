8257c3e7768e137531e9af6b8f9ef8a6acb23578
Stefano <stefano.vanin@wartsila.com>
2023-12-28 16:02:44 +0100

    feat(add-basic-tests): add model_validator on Integrator and method to retrieve ingredients  - ingredients in nutritional_table must be unique  - add method to retrieve a subset of selected ingredients from the nutritional_table field

--- src/integra_mix/models.py
+++ src/integra_mix/models.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 from integra_mix.quantities import Quantity
 
@@ -19,3 +19,20 @@ class Integrator(BaseModel):
     )
     cost: float = Field(default=None, description="cost of the integrator")
     vendor: str = Field(default=None, description="vendor associated to the cost")
+
+    @model_validator(mode="after")
+    def check_unique_ingredient_names(cls, values):
+        names = [ingredient.name for ingredient in values.nutritional_table]
+        if len(names) != len(set(names)):
+            raise ValueError("Ingredient names must be unique")
+        return values
+
+    def get_ingredients(self, ingredient_list: List[str] = []) -> dict[str, Ingredient]:
+        return {
+            key: [
+                ingredient
+                for ingredient in self.nutritional_table
+                if ingredient.name == key
+            ][0]
+            for key in ingredient_list
+        }
